20147a2e18b0b5951976c68820fe5d43ea99bde2
freshad22 <113464976+freshad22@users.noreply.github.com>
2023-03-13 18:34:17 +0000

    Deploying to gh-pages from @ freshad22/goit-js-hw-09@598856061564cace4c2db5088faf3a071b9dfe2a 🚀

---
+++ 01-color-switcher.77f9206d.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.disabled=!0;var n=null;t.addEventListener("click",(function(){n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.77f9206d.js.map
